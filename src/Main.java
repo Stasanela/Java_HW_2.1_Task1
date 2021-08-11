b3393cccaea041ff554a831b8ba2bffe7c70742e
stasanela <stasanela@yandex.ru>
2021-08-11 16:28:04 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 12_969;
+        int bonus = 20;
+        int miles = price / bonus;
+        System.out.println(miles);
+
+    }
+}
